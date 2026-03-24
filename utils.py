95e0daee383d48ecb46786d3f7b7c0578f21a142
MauroMigli <mauro.migliaro@uc.cl>
2026-03-24 19:49:18 -0300

    refactorizacion e inicio del fin

--- utils.py
+++ utils.py
@@ -1,42 +1,45 @@
 import numpy as np
 from statsmodels.tsa.api import VAR
 
+#################################
+###### REQUIERE ACTUALIZAR ######
+#################################
 
-# Metodo AIC para encontrar p
-# minimiza valores del arreglo AIC
-def AIC(data, n_epochs, m, n):
+
+def AIC_BIC(data, n_epochs, m, T) -> tuple:
     lags = range(1, 21)
     AIC = np.zeros(20)
-
+    BIC = np.zeros(20)
+    
 
     for epoch in range(n_epochs):
         data_epoch = data[epoch].T
         model = VAR(data_epoch)
         
-        print(f"Epoca: {epoch}")
         for lag in lags:
-            print(f"Lag: {lag}")
 
             _, log_det_v = np.linalg.slogdet(model.fit(maxlags=lag).sigma_u) # sign, log_det_v
 
-            # AIC[p] = 2k - 2 ln(L_hat)
+            # AIC[p] = 2k + 2 ln(L_hat)
             # k = p * m**2 es el n. de parametros
-            # L_hat ≈ det(V) donde V es la matriz de covarianza del error (gaussiano)
-            aic = log_det_v + (2 * lag * (m**2)) / n
+            # L_hat ≈ 1/det(V) donde V es la matriz de covarianza del error (gaussiano)
+            aic = log_det_v * T + (2 * lag * (m**2))
+            bic = log_det_v * T + np.log(T) * lag * (m**2)
             AIC[lag - 1] += aic / n_epochs
+            BIC[lag - 1] += bic / n_epochs
     
-    return lags[np.argmin(AIC)]
+    return lags[np.argmin(AIC)], lags[np.argmin(BIC)]
 
 
 # Calculo manual, para cada epoca, de 
-def V_epoch(data, A, p, epoch, n_nodes, n_samples):
-    data_epoch = data[epoch]
+def V_epoch(data_epoch, A_epoch, p, n_nodes, n_samples):
+    
 
     X_hats = np.zeros((n_nodes, n_samples - p))
     
     for k in range(p):
         X_prevs = data_epoch[:, p - (k + 1) : n_samples - (k + 1)]
-        X_hats += A[epoch][k] @ X_prevs
+        X_hats += A_epoch[k] @ X_prevs
 
     Xs = data_epoch[:, p:]
 
